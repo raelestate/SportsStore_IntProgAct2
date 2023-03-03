9f8bba98394141b9c2cd380344b0ee6906eb7053
RaelEstate <76102379+raelestate@users.noreply.github.com>
2023-03-03 10:32:29 +0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Neri_SportsStore.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index() => View();
+
+	}
+}
---
+++ Models/EFStoreRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Neri_SportsStore.Models
+{
+	public class EFStoreRepository : IStoreRepository
+	{
+		private StoreDbContext context;
+
+		public EFStoreRepository(StoreDbContext ctx)
+		{
+			context = ctx;
+		}
+
+		public IQueryable<Product> Products => context.Products;
+	}
+}
